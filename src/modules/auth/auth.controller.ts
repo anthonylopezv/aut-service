1ee2655615ad50065f2909a22275d43c701504f8
shisui3009 <anthony.lopez@unmsm.edu.pe>
2019-04-24 21:42:30 -0500

    service auth ok

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { AuthService } from "./auth.service";
 import { User } from "./interfaces/auth.interface";
-import { async } from 'rxjs/internal/scheduler/async';
+import { CreateUserDto } from "./dto/created-user.dto";
 
 @Controller('users')
 export class AuthController {
@@ -15,7 +15,12 @@ export class AuthController {
   }
 
   @Post('/login')
-  async signin(@Body() username: string, @Body() password: string) {
-    return await this.authService.signin(username, password);
+  async signin(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signin(createUserDto);
+  }
+
+  @Post()
+  async createdUser(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.createdUser(createUserDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Inject, OnModuleInit } from "@nestjs/common";
 import { ClientProxy, MessagePattern } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { User } from "./interfaces/auth.interface";
+import { CreateUserDto } from './dto/created-user.dto';
 
 import { JwtService } from '@nestjs/jwt';
 
@@ -17,10 +18,10 @@ export class AuthService {
     await this.client.connect();
   }
 
-  async signin(username: String, password: String) {
+  async signin(createUserDto: CreateUserDto) {
     const pattern = { cmd: 'FIND_USERS' };
     const users = await this.client.send<User[]>(pattern, []).toPromise();
-    
+    const {username, password} = createUserDto;
     const user = users.find(u => u.username === username)
 
     if (!user) return 'User no found';
@@ -38,5 +39,11 @@ export class AuthService {
     const pattern = { cmd: 'FIND_USERS' };
     const users = await this.client.send<User[]>(pattern, []).toPromise();
     return users;
+  }
+
+  async createdUser(createUserDto: CreateUserDto) {
+    const pattern = { cmd: 'CREATED_USER' };
+    const newUser = await this.client.send<User[]>(pattern, createUserDto).toPromise();
+    return newUser;
   }
 }
